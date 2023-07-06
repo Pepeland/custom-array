19b1ad27a347859c956eb5d51982d9bd9b09a5da
Hamidreza Gharahzadeh <hamidprime@gmail.com>
2023-07-06 20:50:35 +0330

    Create custom-array.js

---
+++ custom-array.js
@@ -0,0 +1,27 @@
+function array(...items) {
+    this.items = items;
+    this.push = function (item) {
+        this.items.push(item);
+    }
+    this.pop = function () {
+        if(this.items.length === 0) return null;
+        return this.items.splice(this.items.length - 1, 1);
+    }
+    this.filter = function (f) {
+        const result = [];
+        this.items.forEach(i => {
+            if (f(i))
+                result.push(i);
+        })
+        return result;
+    }
+    return this;
+}
+
+const friends = array("Ben", "Lynn", "Alex");
+friends.push("Mikenzi");
+console.log(friends.pop());
+const f = friends.filter(friend => {
+    return friend.length === 4;
+})
+console.log(f);
